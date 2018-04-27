2f6585d4631ee1a1b81d9c0cd806f09566042392
Tyler Bettilyon <teb311@gmail.com>
2018-04-27 12:53:39 -0700

    added start stop button and auto scaling

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 // Main Entry Point:
 document.addEventListener("DOMContentLoaded", function(event) {
-  console.log('Use window.sim.start() and window.sim.stop() to start/stop the simulation');
   let pixelSize = 10;
-  let roundDelay = 10;
+  let roundDelay = 100;
   let chanceOfLife = .2
-  let rows = (window.outerHeight / pixelSize);
-  let cols = (window.outerWidth / pixelSize);
-  window.sim = new Simulation(rows, cols, pixelSize, pixelSize, chanceOfLife);
-  document.body.append(window.sim.canvas);
-  window.sim.start();
+  let rows = (window.outerHeight * .75) / pixelSize;
+  let cols = (window.outerWidth * .75) / pixelSize;
+  let sim = new Simulation(rows, cols, pixelSize, roundDelay, chanceOfLife);
+  let startStopBut = document.createElement('button');
+  startStopBut.innerHTML = 'Start/Stop';
+  document.body.append(startStopBut);
+  document.body.append(sim.canvas);
+
+  startStopBut.addEventListener('click', () => {
+    if(sim.intervalId) {
+      sim.stop();
+    }
+    else {
+      sim.start();
+    }
+  });
 });
